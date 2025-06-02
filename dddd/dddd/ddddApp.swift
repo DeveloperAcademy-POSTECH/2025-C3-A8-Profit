fead26c4efcfbc15359a709aa909446e0d58230a
Jijoo Maeng <mjiju06@gmail.com>
2025-06-02 17:43:29 +0900

    feat: intial commit

---
+++ dddd/dddd/ddddApp.swift
@@ -0,0 +1,17 @@
+//
+//  ddddApp.swift
+//  dddd
+//
+//  Created by JiJooMaeng on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ddddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
