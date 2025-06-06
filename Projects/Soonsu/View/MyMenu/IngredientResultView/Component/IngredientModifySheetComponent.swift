7242b87180a204ed0f44f9b59ad80e725ecf6ed8
Jung-creator <junchoia25@gmail.com>
2025-06-06 11:54:07 +0900

    feat: 바텀시트에 삭제, 수정 버튼 추가

--- Projects/Soonsu/View/MyMenu/IngredientResultView/Component/IngredientModifySheetComponent.swift
+++ Projects/Soonsu/View/MyMenu/IngredientResultView/Component/IngredientModifySheetComponent.swift
@@ -17,40 +17,51 @@ struct IngredientModifySheet: View {
         case auto = "자동계산"
         case manual = "직접입력"
     }
-
+    
     var body: some View {
-        VStack(spacing: 16) {
-
-            
-            Picker("모드", selection: $segmentMode) {
-                ForEach(SegmentMode.allCases, id: \.self) {
-                    segmentMode in Text(segmentMode.rawValue).tag(segmentMode)
+        NavigationStack {
+            VStack(spacing: 16) {
+                
+                Picker("모드", selection: $segmentMode) {
+                    ForEach(SegmentMode.allCases, id: \.self) {
+                        segmentMode in Text(segmentMode.rawValue).tag(segmentMode)
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                Image(systemName: "photo")
+                    .font(.system(size: 48))
+                    .foregroundStyle(Color.gray.opacity(0.2))
+                
+                Text(ingredient.name)
+                    .font(.title)
+                    .bold()
+                
+                if segmentMode == .auto {
+                    Text("수량: \(ingredient.amount)")
+                    Text("단가: \(ingredient.unitPrice.formatted())원")
+                } else {
+                    TextField("수량입력", text: $customAmount)
+                        .textFieldStyle(.plain)
+                        .keyboardType(.default)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .onAppear {
+                customAmount = ingredient.amount
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Text("삭제")
+                        .foregroundStyle(Color.red)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Text("수정")
+                        .foregroundStyle(Color.blue)
                 }
             }
-            .pickerStyle(.segmented)
-            
-            Image(systemName: "photo")
-                .font(.system(size: 48))
-                .foregroundStyle(Color.gray.opacity(0.2))
-            
-            Text(ingredient.name)
-                .font(.title)
-                .bold()
-            
-            if segmentMode == .auto {
-                Text("수량: \(ingredient.amount)")
-                Text("단가: \(ingredient.unitPrice.formatted())원")
-            } else {
-                TextField("수량입력", text: $customAmount)
-                    .textFieldStyle(.plain)
-                    .keyboardType(.default)
-            }
-
-            Spacer()
-        }
-        .padding()
-        .onAppear {
-            customAmount = ingredient.amount
         }
     }
 }
