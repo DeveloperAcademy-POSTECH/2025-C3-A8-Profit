450011098e4c50456eb65f3b32bd36441a53499c
Jijoo Maeng <mjiju06@gmail.com>
2025-06-07 20:35:58 +0900

    feat: 인건비 관리뷰 추가

---
+++ Projects/Soonsu/View/FixedCostManage/LaborCostEditView.swift
@@ -0,0 +1,18 @@
+//
+//  LaborCostEditView.swift
+//  Soonsu
+//
+//  Created by JiJooMaeng on 6/7/25.
+//
+
+import SwiftUI
+
+struct LaborCostEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaborCostEditView()
+}
---
+++ Projects/Soonsu/View/FixedCostManage/OverheadCostManageView.swift
@@ -0,0 +1,18 @@
+//
+//  OverheadCostManageView.swift
+//  Soonsu
+//
+//  Created by JiJooMaeng on 6/6/25.
+//
+
+import SwiftUI
+
+struct OverheadCostManageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OverheadCostManageView()
+}
