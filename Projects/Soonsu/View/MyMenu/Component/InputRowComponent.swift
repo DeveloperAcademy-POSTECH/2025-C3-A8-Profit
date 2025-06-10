e4ff83c0adde6af7d763b5d00015f3ae94ae1f5a
Jung-creator <junchoia25@gmail.com>
2025-06-10 16:55:00 +0900

    fix: menuInputView 뷰디자인 복구

--- Projects/Soonsu/View/MyMenu/Component/InputRowComponent.swift
+++ Projects/Soonsu/View/MyMenu/Component/InputRowComponent.swift
@@ -4,64 +4,69 @@
 ////
 ////  Created by Choi Jung In on 6/7/25.
 ////
-//
-//import SwiftUI
-//
-//struct InputRowComponent: View {
-//    let title: String
-//    var placeholder: String = ""
-//    @Binding var text: String
-//    var keyboardType: UIKeyboardType = .default
-//    var focusedField: FocusState<MenuInputView.Field?>.Binding?
-//    var isEnabled: Bool = true
-//    var unit: String? = nil
-//
-//    var body: some View {
-//
-//        VStack {
-//            HStack(spacing: 0) {
-//                Text(title)
-//                    .font(.body)
-//                    .fontWeight(.bold)
-//                    .foregroundStyle(.gray)
-//                Spacer()
-//                if let binding = focusedField {
-//                    TextField(placeholder, text: $text)
-//                        .multilineTextAlignment(.trailing)
-//                        .foregroundStyle(isEnabled ? .blue : .black)
-//                        .font(.body)
-//                        .fontWeight(.bold)
-//                        .keyboardType(keyboardType)
-//                        .focused(binding, equals: title == "메뉴 이름" ? .menuName : .menuPrice)
-//                        .disabled(!isEnabled)
-//                } else {
-//                    TextField(placeholder, text: $text)
-//                        .multilineTextAlignment(.trailing)
-//                        .foregroundStyle(isEnabled ? .blue : .black)
-//                        .font(.body)
-//                        .fontWeight(.bold)
-//                        .keyboardType(keyboardType)
-//                        .disabled(!isEnabled)
-//                }
-//                if let unit {
-//                    Text(unit)
-//                        .foregroundStyle(.blue)
-//                        .font(.body)
-//                        .fontWeight(.bold)
-//                } else {
-//                    Text("g")
-//                        .foregroundStyle(isEnabled ? .blue : .black)
-//                        .font(.body)
-//                        .fontWeight(.bold)
-//                }
-//            }
-//            .padding(.horizontal)
-//            .padding(.vertical,4)
-//        }
-//
-//    }
-//}
-//
+
+import SwiftUI
+
+enum Field: Hashable {
+        case menuName
+        case menuPrice
+    }
+
+struct InputRowComponent: View {
+    let title: String
+    var placeholder: String = ""
+    @Binding var text: String
+    var keyboardType: UIKeyboardType = .default
+    var focusedField: FocusState<Field?>.Binding?
+    var isEnabled: Bool = true
+    var unit: String? = nil
+
+    var body: some View {
+
+        VStack {
+            HStack(spacing: 0) {
+                Text(title)
+                    .font(.body)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.gray)
+                Spacer()
+                if let binding = focusedField {
+                    TextField(placeholder, text: $text)
+                        .multilineTextAlignment(.trailing)
+                        .foregroundStyle(isEnabled ? .blue : .black)
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .keyboardType(keyboardType)
+                        .focused(binding, equals: title == "메뉴 이름" ? .menuName : .menuPrice)
+                        .disabled(!isEnabled)
+                } else {
+                    TextField(placeholder, text: $text)
+                        .multilineTextAlignment(.trailing)
+                        .foregroundStyle(isEnabled ? .blue : .black)
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .keyboardType(keyboardType)
+                        .disabled(!isEnabled)
+                }
+                if let unit {
+                    Text(unit)
+                        .foregroundStyle(.blue)
+                        .font(.body)
+                        .fontWeight(.bold)
+                } else {
+                    Text("g")
+                        .foregroundStyle(isEnabled ? .blue : .black)
+                        .font(.body)
+                        .fontWeight(.bold)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.vertical,4)
+        }
+
+    }
+}
+
 //
 //#Preview {
 //    struct Preview: View {
